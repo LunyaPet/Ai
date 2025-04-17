7b4638c055b398da2fe0c769f330b5b05759872a
Lunya <mldkyt@proton.me>
2025-04-17 23:55:36 +0200

    feat: Fixed statuses

--- cogs/dev_commands.py
+++ cogs/dev_commands.py
@@ -2,11 +2,13 @@ import asyncio
 import os
 
 import discord
+import sentry_sdk
 
 from cogs.verification import StartVerificationButton
 from util.storage import get_data, set_data
 
 GUILD = os.getenv("GUILD")
+OWNER = os.getenv("OWNER")
 
 class DevCommands(discord.Cog):
     def __init__(self, bot: discord.Bot):
@@ -19,7 +21,22 @@ class DevCommands(discord.Cog):
         # Set status if in configuration file
         status_data = get_data("status")
         if "mode" in status_data and "status" in status_data:
-            await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType[status_data["mode"]], status=status_data["status"]))
+            activity_type = discord.ActivityType.playing
+            if status_data["mode"] == "watching":
+                activity_type = discord.ActivityType.watching
+            elif status_data["mode"] == "streaming":
+                activity_type = discord.ActivityType.streaming
+            elif status_data["mode"] == "competing":
+                activity_type = discord.ActivityType.competing
+            elif status_data["mode"] == "listening":
+                activity_type = discord.ActivityType.listening
+
+            activity = discord.Activity(
+                type=activity_type,
+                name=status_data["status"]
+            )
+
+            await self.bot.change_presence(activity=activity)
 
     dev_group = discord.SlashCommandGroup(name="dev", description="Developer commands", guild_ids=[int(GUILD)],
         default_member_permissions=discord.Permissions(administrator=True), )
@@ -30,17 +47,46 @@ class DevCommands(discord.Cog):
 
     @dev_group.command(name="send_verify_message")
     async def send_verify_cmd(self, ctx: discord.ApplicationContext):
-        await asyncio.gather(ctx.channel.send(
-            "# Verify\nIn order to access the rest of this server, you must verify. Please click the \"start verify\" button in order to begin!",
-            view=StartVerificationButton()), ctx.respond("Sent", ephemeral=True))
+        try:
+            if ctx.user.id != int(OWNER):
+                await ctx.respond("You are not authorized to use this command!", ephemeral=True)
+                return
+
+            await asyncio.gather(ctx.channel.send(
+                "# Verify\nIn order to access the rest of this server, you must verify. Please click the \"start verify\" button in order to begin!",
+                view=StartVerificationButton()), ctx.respond("Sent", ephemeral=True))
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
 
     @dev_group.command(name="set_status")
     @discord.option(name="mode", description="The mode to set the status to", type=str, choices=["watching", "streaming", "playing", "competing", "listening"])
     async def set_status(self, ctx: discord.ApplicationContext, mode: str, status: str):
-        existing_data = get_data("status")
-        existing_data["mode"] = mode
-        existing_data["status"] = status
-        set_data("status", existing_data)
+        try:
+            if ctx.user.id != int(OWNER):
+                await ctx.respond("You are not authorized to use this command!", ephemeral=True)
+                return
 
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType[mode], status=status))
-        await ctx.respond(f"Status set to {mode}: {status}!", ephemeral=True)
+            existing_data = get_data("status")
+            existing_data["mode"] = mode
+            existing_data["status"] = status
+            set_data("status", existing_data)
+
+            activity_type = discord.ActivityType.playing
+            if mode == "watching":
+                activity_type = discord.ActivityType.watching
+            elif mode == "streaming":
+                activity_type = discord.ActivityType.streaming
+            elif mode == "competing":
+                activity_type = discord.ActivityType.competing
+            elif mode == "listening":
+                activity_type = discord.ActivityType.listening
+
+            activity = discord.Activity(
+                type=activity_type,
+                name=status
+            )
+
+            await self.bot.change_presence(activity=activity)
+            await ctx.respond(f"Status set to {mode}: {status}!", ephemeral=True)
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
