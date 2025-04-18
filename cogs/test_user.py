5f2431c8676fd6e467a9d55ad8458e98ae73905b
Lunya <mldkyt@proton.me>
2025-04-18 16:53:50 +0200

    feat: Add message command

--- cogs/test_user.py
+++ cogs/test_user.py
@@ -8,3 +8,11 @@ class UserCommands(discord.Cog):
     @discord.command(name='ping', description='Tests connection to Discord', integration_types=[discord.IntegrationType.user_install])
     async def ping(self, ctx: discord.ApplicationContext):
         await ctx.respond(f'Pong! {self.bot.latency * 1500} ms', ephemeral=(ctx.user.id != int(OWNER)))
+
+    @discord.command(name='message', description='Send message as bot', integration_types=[discord.IntegrationType.user_install])
+    async def ping(self, ctx: discord.ApplicationContext, msg: str):
+        if ctx.user.id != int(OWNER):
+            await ctx.respond("You are not authorized to use this command!", ephemeral=True)
+            return
+
+        await ctx.respond(msg)
