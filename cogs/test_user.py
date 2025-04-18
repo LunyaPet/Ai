b7c204c7f47a7d57d6a99d49672ab270351f6510
Lunya <mldkyt@proton.me>
2025-04-18 16:50:37 +0200

    feat: User command test

--- cogs/test_user.py
+++ cogs/test_user.py
@@ -5,9 +5,6 @@ class UserCommands(discord.Cog):
     def __init__(self, bot: discord.Bot):
         self.bot = bot
 
-    @discord.slash_command(name='ping', description='Tests connection to Discord', contexts=[
-        discord.InteractionContextType.bot_dm,
-        discord.InteractionContextType.private_channel
-    ])
+    @discord.command(name='ping', description='Tests connection to Discord', integration_types=[discord.IntegrationType.user_install])
     async def ping(self, ctx: discord.ApplicationContext):
-        await ctx.respond('Pong!', ephemeral=(ctx.user.id != int(OWNER)))
+        await ctx.respond(f'Pong! {self.bot.latency * 1500} ms', ephemeral=(ctx.user.id != int(OWNER)))
