3a82a53f6354b055ed664556eb031ad31235d91f
Lunya <mldkyt@proton.me>
2025-04-19 13:09:41 +0200

    feat: User commands: Lookup fedi note and user

--- cogs/user_commands.py
+++ cogs/user_commands.py
@@ -1,13 +1,12 @@
 import random
-from idlelib.window import add_windows_to_menu
-
-import discord
-import sentry_sdk
 import subprocess
 
+import aiohttp
+import discord
+import sentry_sdk
 from discord import Interaction
 
-from constants import OWNER
+from constants import OWNER, FEDI_INSTANCE
 
 
 class MeowComponent(discord.ui.View):
@@ -45,6 +44,120 @@ class SillyComponent(discord.ui.View):
     @discord.ui.button(label=":3", style=discord.ButtonStyle.primary)
     async def silly(self, button: discord.ui.Button, interaction: discord.Interaction):
         await interaction.response.send_modal(SillyModal(self.title, self.label, self.placeholder))
+
+async def lookup_user(user_name: str, user_inst: str | None) -> list[discord.Embed] | None:
+    async with aiohttp.ClientSession() as session:
+        body = {
+            "username": user_name,
+        }
+
+        if user_inst is not None:
+            body["host"] = user_inst
+
+        async with session.post(f"https://{FEDI_INSTANCE}/api/users/show", json=body) as resp:
+            if resp.status != 200:
+                return None
+
+            resp_body = await resp.json()
+
+            host = FEDI_INSTANCE if user_inst is None else user_inst
+            desc = resp_body["description"][:1500]
+            if desc != resp_body["description"]:
+                desc = desc + '...'
+
+            emb = discord.Embed(
+                title=f"{resp_body['name']} (@{resp_body['username']}@{host})",
+                description=desc,
+                thumbnail=resp_body["avatarUrl"],
+                fields=[
+                    discord.EmbedField(name="Notes", value=resp_body["notesCount"], inline=True),
+                    discord.EmbedField(name="Followers", value=resp_body["followersCount"], inline=True),
+                    discord.EmbedField(name="Following", value=resp_body["followingCount"], inline=True)
+                ],
+                url=f'https://{FEDI_INSTANCE}/@{resp_body["username"]}@{host}',
+                color=discord.Color.from_rgb(255, 119, 255)
+            )
+
+            if user_inst is not None:
+                emb.set_footer(text="The information may not be accurate as it comes from a remote instance.")
+
+            pinned_notes = []
+            i = 0
+            for post in resp_body["pinnedNotes"]:
+                i = i + 1
+                if i > 2:
+                    break
+
+                pinned_notes.extend(await lookup_note_id(post["id"], pinned=True))
+
+            return [emb, *pinned_notes]
+
+
+async def lookup_note_id(note_id: str, pinned: bool = False) -> list[discord.Embed] | None:
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f"https://{FEDI_INSTANCE}/api/notes/show", json={
+            "noteId": note_id
+        }) as resp:
+            if resp.status != 200:
+                return None
+
+            resp_body = await resp.json()
+
+            image_url = resp_body["files"][0]["url"] if len(resp_body["files"]) > 0 else None
+
+            reaction_string = ""
+
+            if resp_body["repliesCount"] > 0:
+                reaction_string = f'{resp_body["repliesCount"]} replies | '
+
+            if resp_body['renoteCount'] > 0:
+                reaction_string = f'{resp_body["renoteCount"]} renotes | '
+
+            for k, v in resp_body["reactions"].items():
+                if len(k.split("@")) > 1:
+                    k = f'{k.split("@")[0]}:'
+
+                reaction_string += f'{v}x {k} | '
+
+            reaction_string = reaction_string[:-3]
+
+            host = FEDI_INSTANCE if resp_body['user']['host'] is None else resp_body['user']['host']
+
+            pinned_post = "Pinned note by " if pinned else ""
+
+            emb = discord.Embed(
+                title=f"{'Pinned ' if pinned else ''}Note by {resp_body['user']['name']} (@{resp_body['user']['username']}@{host})",
+                url=f"https://{FEDI_INSTANCE}/notes/{resp_body['id']}",
+                description=resp_body["text"] if resp_body["cw"] is None else f'CW: {resp_body["cw"]}',
+                image=image_url,
+                author=discord.EmbedAuthor(
+                    name=f"{pinned_post}{resp_body['user']['name']} (@{resp_body['user']['username']}@{host})",
+                    icon_url=resp_body["user"]["avatarUrl"],
+                    url=f'https://{FEDI_INSTANCE}/@{resp_body["user"]["username"]}@{host}'
+                ),
+                color=discord.Color.from_rgb(255, 119, 255),
+                footer=discord.EmbedFooter(
+                    text=reaction_string
+                ) if len(reaction_string) > 0 else None
+            )
+
+            return [emb]
+
+
+async def lookup_by_str(lookup_str: str) -> list[discord.Embed] | None:
+
+    user_mention = lookup_str.split("@")
+    if len(user_mention) == 2:
+        # Local user lookup
+        return await lookup_user(user_mention[1], None)
+    elif len(user_mention) == 3:
+        # Remote user lookup
+        return await lookup_user(user_mention[1], user_mention[2])
+
+    # Lookup note by MisskeyID
+
+    return await lookup_note_id(lookup_str)
+
 
 class UserCommands(discord.Cog):
     def __init__(self, bot: discord.Bot):
@@ -92,3 +205,20 @@ class UserCommands(discord.Cog):
             return
 
         await ctx.respond(text, view=SillyComponent(title, label, placeholder))
+
+    fedi_group = discord.SlashCommandGroup(name="fedi", description="Fedi commands", integration_types=[discord.IntegrationType.user_install])
+
+    @fedi_group.command(name="lookup", description="Lookup fedi user/post")
+    async def lookup_post(self, ctx: discord.ApplicationContext, lookup_str: str):
+        try:
+            # Let's attempt generate embed based on lookup function
+            emb = await lookup_by_str(lookup_str)
+
+            if emb is None:
+                await ctx.respond("Failed to lookup!", ephemeral=True)
+                return
+
+            await ctx.respond(embeds=emb)
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
+            await ctx.respond("Error when looking up!", ephemeral=True)
