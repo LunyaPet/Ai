1b4db0d4509d89a95de3b7dfc1b7c4c51fcaa176
mld <mldkyt@proton.me>
2025-05-06 08:26:02 +0200

    feat: Better auto response

--- cogs/funny_auto_response.py
+++ cogs/funny_auto_response.py
@@ -8,16 +8,16 @@ import sentry_sdk
 from constants import GUILD
 
 
-async def handle_colon_three(message: discord.Message):
+def handle_colon_three(message: discord.Message):
     # count :3 in message
     count = 0
     for i in re.finditer(r":(3+)", message.content.lower()):
         count = count + len(i[1])
 
     if count == 0:
-        return
+        return ""
 
-    await message.channel.send(":3 " * count)
+    return ":3 " * count
 
 def generate_meow():
     meow = random.choice(["meow", "nya", "mraow", "mwrmmwra"])
@@ -34,24 +34,33 @@ def generate_meow():
     return meow
 
 
-async def handle_meow(message: discord.Message):
+def handle_meow(message: discord.Message):
     # count meows
     count = len(re.findall(r"(m[mraeow]{3,}|nya+)", message.content.lower()))
 
     if count == 0:
-        return
+        return ""
 
-    await message.channel.send(" ".join([generate_meow() for _ in range(count)]) + " :3")
+    return " ".join([generate_meow() for _ in range(count)]) + " :3"
 
 
-async def handle_owo(message: discord.Message):
+def handle_owo(message: discord.Message):
     # count OwOs and UwUs
     count = len(re.findall(r"([ou>]w[<ou])", message.content.lower()))
 
     if count == 0:
-        return
+        return ""
 
-    await message.channel.send(" ".join(random.choices(["OwO", "owo", "UwU", "uwu", ">w<"], k=count)) + " :3")
+    return " ".join(random.choices(["OwO", "owo", "UwU", "uwu", ">w<"], k=count)) + " :3"
+
+
+def handle_gex(message: discord.Message):
+    count_say_gex = len(re.findall(r"say gex", message.content.lower()))
+    count_sesbian_lex = len(re.findall(r"sesbian lex", message.content.lower()))
+
+    resp = "say gex " * count_say_gex
+    resp += "sesbian lex " * count_sesbian_lex
+    return resp.strip()
 
 
 class FunnyAutoResponse(discord.Cog):
@@ -67,10 +76,16 @@ class FunnyAutoResponse(discord.Cog):
             if message.guild.id != int(GUILD):
                 return
 
-            await asyncio.gather(
+            message_reply = " ".join([
                 handle_meow(message),
                 handle_colon_three(message),
-                handle_owo(message)
-            )
+                handle_owo(message),
+                handle_gex(message)
+            ])
+
+            message_reply = message_reply.replace("  ", " ")
+
+            if len(message_reply.strip()) > 0:
+                await message.channel.send(message_reply)
         except Exception as e:
             sentry_sdk.capture_exception(e)
