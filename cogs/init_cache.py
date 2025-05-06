0fff395d635472564b0b76944449b6a408f19521
Lunya <mldkyt@proton.me>
2025-05-06 21:32:17 +0200

    feat: Init cache asychronously

--- cogs/init_cache.py
+++ cogs/init_cache.py
@@ -30,12 +30,18 @@ class InitCache(discord.Cog):
                 return
 
             # Init channels
-            await asyncio.gather(*[self.init_channel(i) for i in guild.channels if not isinstance(i, CategoryChannel)])
+
+            for i in guild.text_channels:
+                asyncio.get_running_loop().create_task(self.init_channel(i), name=f"InitCache Text Channel #{i.name} ({i.id})")
+
+            print(f"[InitCache]Created {len(guild.text_channels)} tasks")
 
             print("[InitCache]Init cache", flush=True)
             print("[InitCache]Init DM cache", flush=True)
             # DM channels
-            await asyncio.gather(*[self.init_channel(i) for i in self.bot.private_channels])
+            for i in self.bot.private_channels:
+                asyncio.get_running_loop().create_task(self.init_channel(i), name=f"InitCache Private Channel {i.id}")
+            print(f"[InitCache]Created {len(self.bot.private_channels)} tasks")
             print("[InitCache]Init DM cache done..", flush=True)
         except Exception as e:
             sentry_sdk.capture_exception(e)
